9d0e9f4975de9b6d5651845b369af7cdeabc4ae4
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-06 14:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,53 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+
+	const method=video.paused?'play':'pause'
+
+	video[method]();
+}
+
+function toggleButton(){
+
+	const icon=this.paused?'►':'❚ ❚'
+
+	toggle.textContent=icon;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function handlePlaybackSpeedChange() {
+  video.playbackRate = this.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+
+
+
+video.addEventListener('click',togglePlay)
+
+toggle.addEventListener('click',togglePlay)
+
+video.addEventListener('play',updateButton)
+
+video.addEventListener('pause',updateButton)
+
+video.addEventListener('timeupdate',handleProgress)
+
+ranges.addEventListener('change', handleVolumeChange);
+
+ranges.addEventListener('mousemove', handleVolumeChange);
+
+playbackSpeed.addEventListener('change', handlePlaybackSpeedChange);
+
+playbackSpeed.addEventListener('mousemove', handlePlaybackSpeedChange);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
